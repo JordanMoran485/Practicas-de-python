f617fbccd60f7816324559ab08b707ea94300aab
JordanMoran485 <jordanarmando310@gmail.com>
2025-05-09 17:48:29 -0500

    aprendiendo clases en python

---
+++ clases.py
@@ -0,0 +1,19 @@
+class Persona:
+    def __init__(self , nombre, apellido, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+    def saludar(self):
+        print("Holaaaaaaa")
+
+    def comer(self):
+        print("Estoy comiendo")
+
+persona1 = Persona("Jordan, Moran, 22")
+persona2 = Persona("Jose, Flores, 23")
+
+persona1.saludar()
+persona2.comer()
+
+print(persona1.)
